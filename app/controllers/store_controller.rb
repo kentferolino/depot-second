b756b2f22d3a5b19c3cea84a209c924a24657572
Kent Ferolino <kentfero@gmail.com>
2023-02-08 15:43:40 +0800

    Able to add products to cart Able to add session counter Add more testing

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,15 @@
 class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
+    set_counter_session
+    @times_store_visited = session[:counter]
+  end
+
+  private
+
+  def set_counter_session
+    return session[:counter] = 1 if session[:counter].nil?
+
+    session[:counter] = session[:counter] + 1
   end
 end
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -55,8 +55,6 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
 
   test "can't delete product in cart" do
     assert_difference('Product.count', 0) do
-      puts "DEBUG HERE"
-      puts products(:two).id
       delete product_url(products(:two))
     end
     assert_redirected_to products_url
